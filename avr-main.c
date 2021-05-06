119fa0e51792cab07a2af7052a0a4bb1f058caab
R1xar <ricardo.lopez.macedo@gmail.com>
2021-05-06 18:53:52 -0500

    Segnda  confirmación

---
+++ avr-main.c
@@ -0,0 +1,17 @@
+/*
+ * File:   avr-main.c
+ * Author: rlmmx
+ *
+ * Created on 27 de abril de 2021, 09:31 PM
+ */
+
+
+#include <avr/io.h>
+
+int main(void) {
+    
+    DDRA =  0;
+    /* Replace with your application code */
+    while (1) {
+    }
+}
