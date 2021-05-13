9af3fdc3248e5921ebd3007638357a0b2e6db967
SergioMar23 <83435523+SergioMar23@users.noreply.github.com>
2021-05-13 16:04:38 -0500

    Sergio Martinez_01

--- avr-main.c
+++ avr-main.c
@@ -1,17 +1,29 @@
 /*
  * File:   avr-main.c
- * Author: rlmmx
+ * Author: Sergio Martinez Lopez
  *
  * Created on 27 de abril de 2021, 09:31 PM
  */
 
 
+#define F_CPU 10000000UL
+#include<util/delay.h>
+#include <xc.h>
 #include <avr/io.h>
 
-int main(void) {
+
+int main(void)
+{
+    DDRB=0x01;
+    PORTB=0b00000001;
     
-    DDRA =  0;
-    /* Replace with your application code */
-    while (1) {
+    while (1)
+    {
+       PORTB=0b00000001;
+       _delay_ms(3000);
+       PORTB=0b00000001;
+        _delay_ms(3000);
+               
     }
+
 }
